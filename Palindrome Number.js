2ba9f46836d78d4e79a470a4455ac739225375ae
muhamadd9 <mhmdramadan.192@gmail.com>
2024-06-21 02:44:22 +0300

    Palindrome Number

--- Palindrome Number.js
+++ Palindrome Number.js
@@ -3,6 +3,7 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
+    if(x<0) return false
     const stringOfX = x.toString()
     return stringOfX.split("").reverse().join("") === stringOfX
 };
