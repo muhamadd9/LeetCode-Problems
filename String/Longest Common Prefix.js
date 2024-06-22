714b2e5e9816a52654087bd040f5d94970e6b6e5
muhamadd9 <mhmdramadan.192@gmail.com>
2024-06-22 21:06:12 +0300

    Longest Common Prefix

--- String/Longest Common Prefix.js
+++ String/Longest Common Prefix.js
@@ -1,6 +1,6 @@
 var longestCommonPrefix = function(strs) {
-    let minLength = Math.min(...strs.map((item)=>item.length))
     strs.sort((a,b)=>a.length - b.length)
+    let minLength = strs[0].length
     
     for(let i = minLength ; i >0 ;i-- ){
        let commonPerifix = strs[0].slice(0,i)
