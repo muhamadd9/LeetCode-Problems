6c0ed891da4f46b152de5c9f961f425808ebfaf1
muhamadd9 <mhmdramadan.192@gmail.com>
2024-06-25 05:38:54 +0300

    Find Words Containing Character

---
+++ Array/Find Words Containing Character.js
@@ -0,0 +1,7 @@
+var findWordsContaining = function(words, x) {
+    let newArr = []
+    for(let i =0 ; i<words.length;i++){
+      if(words[i].includes(x)) newArr.push(i)
+    }
+    return newArr
+};
