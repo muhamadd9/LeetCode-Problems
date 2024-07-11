a62ffe405d2adaeae73cd3537addaf70705c20f8
muhamadd9 <mhmdramadan.192@gmail.com>
2024-07-11 20:57:39 +0300

    Left and Right Sum Differences

---
+++ Array/Left and Right Sum Differences.js
@@ -0,0 +1,20 @@
+var leftRightDifference = function(nums) {
+    let n = nums.length;
+    let leftArr = new Array(n).fill(0);
+    let rightArr = new Array(n).fill(0);
+
+    for (let i = 1; i < n; i++) {
+        leftArr[i] = leftArr[i - 1] + nums[i - 1];
+    }
+
+    for (let i = n - 2; i >= 0; i--) {
+        rightArr[i] = rightArr[i + 1] + nums[i + 1];
+    }
+
+    let result = new Array(n);
+    for (let i = 0; i < n; i++) {
+        result[i] = Math.abs(leftArr[i] - rightArr[i]);
+    }
+
+    return result;
+};
