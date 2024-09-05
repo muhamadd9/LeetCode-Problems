66fd910ed12cd6e8f0983eab9c63e6124710041d
Muhamad Ramadan <149680627+muhamadd9@users.noreply.github.com>
2024-09-06 00:41:58 +0300

    Remove Element

---
+++ Array/Remove Element.js
@@ -0,0 +1,14 @@
+var removeElement = function(nums, val) {
+    let start = 0;
+    let end = nums.length - 1;
+    
+    while (start <= end) {
+        if (nums[start] === val) {
+            nums[start] = nums[end];
+            end--;
+        } else {
+            start++;
+        }
+    }
+    return start;
+};
