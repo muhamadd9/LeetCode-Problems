55ed1d2e80ea2947514803bd06655ed11822a5f1
Muhamad Ramadan <149680627+muhamadd9@users.noreply.github.com>
2024-09-07 10:45:37 +0300

    Create Reverse Vowels of a String.js

---
+++ String/Reverse Vowels of a String.js
@@ -0,0 +1,22 @@
+var reverseVowels = function(s) {
+   const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+   let arrOfVowels = []
+   for(let i = 0 ; i < s.length;  i++){
+    if(vowels.includes(s[i])){
+      arrOfVowels.push(s[i])
+    }
+   }
+   arrOfVowels.reverse()
+    let newString = ''
+    let vowelIndex = 0;
+    for(let i = 0 ; i < s.length;  i++){
+    if(vowels.includes(s[i])){
+      newString += arrOfVowels[vowelIndex];
+      vowelIndex++
+    }
+    else{
+      newString += s[i]
+    }
+   }
+   return newString ;
+};
