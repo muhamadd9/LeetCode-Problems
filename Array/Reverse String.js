90a529161558d4d656a5d0b247e6142705a60f91
Muhamad Ramadan <149680627+muhamadd9@users.noreply.github.com>
2024-09-07 10:20:02 +0300

    Reverse String

--- Array/Reverse String.js
+++ Array/Reverse String.js
@@ -2,3 +2,17 @@ var reverseString = function(s) {
     s = s.reverse() ;
     return s; 
 };
+
+// Solution using 2 pointers ;
+
+var reverseString = function(s) {
+    let start = 0 ; 
+    let end = s.length - 1 ;
+    while(start < end ){
+        [s[start],s[end]] = [s[end],s[start]]
+        start++ ; 
+        end-- ;
+    }
+
+    return s ;
+};
