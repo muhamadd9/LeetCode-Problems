fc86bb4736762964a7af5d229cbbf7ae462d1fc0
muhamadd9 <mhmdramadan.192@gmail.com>
2024-09-08 23:41:15 +0300

    Find Peak Element

---
+++ Binary Search/Find Peak Element.js
@@ -0,0 +1,14 @@
+var findPeakElement = function(nums) {
+    let start = 0 , end = nums.length - 1 ; 
+    while(start < end){
+        let mid = Math.floor((start + end) / 2) ;
+        if(nums[mid] > nums[mid + 1]){
+            end = mid ;
+        }
+        else{
+            start = mid + 1
+        }
+    }
+
+    return start
+};
