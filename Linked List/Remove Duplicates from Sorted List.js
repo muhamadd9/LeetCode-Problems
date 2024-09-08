3a501a73f3dd26aa0591df0fd3f33e577b138279
Muhamad Ramadan <149680627+muhamadd9@users.noreply.github.com>
2024-09-08 15:14:11 +0300

    Remove Duplicates from Sorted List

---
+++ Linked List/Remove Duplicates from Sorted List.js
@@ -0,0 +1,14 @@
+var deleteDuplicates = function(head) {
+    let temp = head ;
+
+    while(temp !== null && temp.next !== null){
+        if(temp.val == temp.next.val){
+            temp.next = temp.next.next; 
+        }
+        else{
+            temp = temp.next
+        }       
+    }
+
+    return head
+};
