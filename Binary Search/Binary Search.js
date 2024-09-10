1d38a6fe410320dd08a8c8c43d8306aca99bf8a4
Muhamad Ramadan <149680627+muhamadd9@users.noreply.github.com>
2024-09-10 22:29:01 +0300

    Binary Search

---
+++ Binary Search/Binary Search.js
@@ -0,0 +1,15 @@
+var search = function(nums, target) {
+    let start = 0 , end = nums.length - 1 ;
+    while(start <= end){
+        let mid = Math.floor((start + end) / 2) ;
+        if(nums[mid] == target) return mid; 
+        else if(nums[mid] > target){
+            end = mid - 1
+        }
+        else{
+            start = mid + 1 ;
+        }
+    }
+
+    return -1 ;
+};
