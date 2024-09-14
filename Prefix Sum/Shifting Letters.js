a4cb5dbe9c803e0649b517c102ea869c5ca4400f
Muhamad Ramadan <149680627+muhamadd9@users.noreply.github.com>
2024-09-14 22:31:28 +0300

    Shifting Letters

---
+++ Prefix Sum/Shifting Letters.js
@@ -0,0 +1,19 @@
+var shiftingLetters = function(s, shifts) {
+      for(let i = 1; i < shifts.length ; i++){
+        shifts[i]+= shifts[i-1]
+      }
+
+      let result = '' ;
+      for(let i = 0 ; i <s.length ; i++){
+        let charCode = s.charCodeAt(i) - 97; 
+        let newCharCode;
+        if(i === 0){
+         newCharCode = (charCode + shifts[shifts.length - 1 ] ) % 26; 
+        }
+        else{
+         newCharCode = (charCode + shifts[shifts.length - 1] - shifts[i-1] ) % 26; 
+        }
+        result += String.fromCharCode(newCharCode + 97)
+      }
+      return result
+};
